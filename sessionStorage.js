aa7e6697727a4dd23a7a994584dc64f4b7120e81
Tsegts-Erdem Delgerduuren <d_tsegts_erdem@yahoo.com>
2023-02-14 13:53:36 +0900

    sessStorage

---
+++ sessionStorage.js
@@ -0,0 +1,5 @@
+sessionStorage.setItem('myCat', 'Tom');
+
+// /from another js or views : can access Item
+const cat = sessionStorage.getItem('myCat');
+console.log("🚀 ~ file: Untitled-1:5 ~ cat", cat)
