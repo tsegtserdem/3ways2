40ffab0212e1ca487db020c54c046064a23303be
Tsegts-Erdem Delgerduuren <d_tsegts_erdem@yahoo.com>
2023-03-01 09:38:57 +0900

    $ operator

---
+++ operator in ${}.js
@@ -0,0 +1,4 @@
+const name = 'fatfish'
+const score = 79
+const result = `${name}${score > 60 ? ' passed the exam' : ' failed in the exam'}`;
+console.log("🚀 ~ file: operator in ${}.js:5 ~ result:", result)
