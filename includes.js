ff7f4d6aead7842b4750dbcd5dea89a211470d89
Tsegts-Erdem Delgerduuren <d_tsegts_erdem@yahoo.com>
2023-03-08 17:41:13 +0900

    update includes

--- includes.js
+++ includes.js
@@ -1,5 +1,10 @@
 const n = 1
-const conditions = [1, 2, 3, 4, 5] // You just need to add new numbers here
+
+// if (n === 1 || n === 2 || n === 3 || n === 4 || n === 5) {
+//   // ...
+// }
+
+const conditions = [1, 2, 3, 4, 5] // add here
 if (conditions.includes(6)) {
     console.log("true");
 }
