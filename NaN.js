c4721f7852c9514474b06950ba92f5665db935c9
Tsegts-Erdem Delgerduuren <d_tsegts_erdem@yahoo.com>
2023-08-07 16:49:57 +0900

    x! == x ?

---
+++ NaN.js
@@ -0,0 +1,7 @@
+const x = NaN // Please fill in the value of "x?
+if (x !== x) {
+  console.log('hello fatfish')
+}
+console.log(NaN === NaN) // false
+console.log(x !== x) // true
+console.log(Number.isNaN(x)) // true
